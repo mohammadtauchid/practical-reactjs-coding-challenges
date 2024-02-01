0acefc0daa3d3b7c8240baacff1eb074b4649cfa
Mohammad Tauchid <muhammadtauchid@gmail.com>
2024-02-01 17:20:38 +0700

    Challenge 3 complete

--- random-quote-generator/src/App.tsx
+++ random-quote-generator/src/App.tsx
@@ -1,35 +1,70 @@
-import classnames from "classnames"
+import { useState } from "react"
 import { ReactComponent as Button } from "../src/assets/icons/button.svg"
 import { ReactComponent as Quotation } from "../src/assets/icons/quotation.svg"
 import { ReactComponent as Twitter } from "../src/assets/icons/twitter.svg"
 import { ReactComponent as Whatsapp } from "../src/assets/icons/whatsapp.svg"
+import axios from "axios"
 import "./App.css"
 
 function App() {
+  const [index, setIndex] = useState<number>(0)
+  const [quotes, setQuotes] = useState<Array<{quote: string, author: string}>>([])
+
+  window.onload = function () {
+    axios.get("http://localhost:4000/quotes")
+      .then((response) => {
+        setQuotes(response.data.sort(() => Math.random() - 0.5))
+      })
+  }
+
+  const encodeString = () => {
+    return encodeURIComponent(
+      quotes[index].quote +
+      "\n" +
+      "- " +
+      quotes[index].author
+    )
+  }
+
   return (
-    <>
+    <>  
       <header>
         <div className="top-strip" />
       </header>
       <div className="container">
-        <div className="quotation-box ">
+        <div className="quotation-box">
           <Quotation />
-          <div className="quote">
-            <p>
-              In the end, we will remember not the words of our enemies, but the
-              silence of our friends.
-            </p>
-            <span>- Martin Luther King Jr.</span>
-          </div>
+          {
+            quotes.length > 0 && (
+              <div className="quote">
+                <p>{quotes[index].quote}</p>
+                <span>- {quotes[index].author}</span>
+              </div>
+            )
+          }
           <div className="bottom-navigation">
             <div>
-              <Button className={classnames("rotate cp")} />
-              <Button className="cp" />
+              <Button 
+                className={`rotate cp ${index === 0 ? "disabled-button" : ""}`} 
+                onClick={() => setIndex(index - 1)} 
+              />
+              <Button 
+                className={`cp ${index === quotes.length - 1 ? "disabled-button" : ""}`} 
+                onClick={() => setIndex(index + 1)}
+              />
             </div>
             <div className="share">
               <span>Share At:</span>
-              <Twitter title="Post this quote on twitter!" className="cp" />
-              <Whatsapp title="Post this quote on WhatsApp!" className="cp" />
+              <Twitter 
+                title="Post this quote on twitter!" 
+                className="cp" 
+                onClick={() => window.open(`https://twitter.com/intent/tweet?text=${encodeString()}`)}
+              />
+              <Whatsapp 
+                title="Post this quote on WhatsApp!" 
+                className="cp" 
+                onClick={() => window.open(`https://wa.me/?text=${encodeString()}`)} 
+              />
             </div>
           </div>
         </div>
